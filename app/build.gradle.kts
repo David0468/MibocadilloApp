7c372061997dc2d4d22204e8c7be87dce2bcc1a7
David0468 <121893529+David0468@users.noreply.github.com>
2025-01-31 19:11:50 +0100

    Login + dependencias + firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("com.google.gms.google-services")
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
@@ -26,6 +27,9 @@ android {
             )
         }
     }
+
+    viewBinding {enable = true}
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,6 +40,27 @@ android {
 }
 
 dependencies {
+    // Room
+    runtimeOnly("androidx.room:room-runtime:2.6.1")
+
+    // Navigation Component
+    runtimeOnly("androidx.navigation:navigation-fragment:2.8.6")
+    runtimeOnly("androidx.navigation:navigation-ui:2.8.6")
+
+    // ViewModel y LiveData
+    runtimeOnly("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    runtimeOnly("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+
+    // Kotlinx Coroutines
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+
+    // RecyclerView
+    runtimeOnly("androidx.recyclerview:recyclerview:1.4.0")
+
+    // FireBase
+    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
